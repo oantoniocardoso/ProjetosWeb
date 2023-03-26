3badfada26505d676a44e5adb4c62818aa229c41
Antonio Cardoso <antonioc_contas@hotmail.com>
2023-03-26 14:15:32 -0300

    Atividade 1 e 2
    Criação de controllers e views dentro do projeto construído em aula.

---
+++ Projeto1/Ftec.ProjetosWeb.Projeto1.Web/Controllers/AlunosController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ftec.ProjetosWeb.Projeto1.Web.Controllers
+{
+    public class AlunosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Listar()
+        {
+            return View();
+        }
+
+        public IActionResult Gravar() 
+        {
+            return View();
+        }
+
+        public IActionResult Excluir() 
+        {
+            return View();
+        }
+
+        public IActionResult Procurar() 
+        {
+            return View();
+        }
+    }
+}
---
+++ Projeto1/Ftec.ProjetosWeb.Projeto1.Web/Controllers/ProdutoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ftec.ProjetosWeb.Projeto1.Web.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Inserir");
+        }
+
+        public IActionResult Inserir()
+        {
+            return View();
+        }
+    }
+}
