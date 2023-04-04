8da8feba412a58ea53eed93bc740878ffd665d69
Antonio Cardoso <antonioc_contas@hotmail.com>
2023-04-03 22:53:51 -0300

    Atividade Aula
    Continuação do projeto 1.

---
+++ Projeto1 Continuação/Ftec.ProjetosWeb.Projeto1.Web/Controllers/ProdutoController.cs
@@ -0,0 +1,30 @@
+﻿using Ftec.ProjetosWeb.Projeto1.Web.Factory;
+using Ftec.ProjetosWeb.Projeto1.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ftec.ProjetosWeb.Projeto1.Web.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public IActionResult Index()
+        {
+            var produtos = ProdutoFactory.GeradorMoqProdutos(5);
+            ViewBag.produtos = produtos;
+            return View();
+        }
+
+        public IActionResult Inserir()
+        {
+            var categias = CategoriaProdutoFactory.GeradorMoqCategoriaProdutos(5);
+            ViewBag.categorias = categias;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Gravar(ProdutoModel produto)
+        {
+            //Faz a gravacao
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Projeto1 Continuação/Ftec.ProjetosWeb.Projeto1.Web/Models/ProdutoModel.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Ftec.ProjetosWeb.Projeto1.Web.Models
+{
+    public class ProdutoModel
+    {
+        public Guid Id { get; set; }
+        public string Descricao { get; set; }
+        public decimal QuantidadeEstoque { get; set; }
+        public decimal Preco { get; set; }
+        public string UnidadeMedida { get; set; }
+        public Guid CategoriaId { get; set; }
+
+        //private string descricao;
+
+       /* public string Descricao
+        {
+            get
+            {
+                return descricao;
+            }
+            set
+            {
+                descricao = value;
+            }
+        }*/
+
+        /*public string getDescricao()
+        {
+            return descricao;
+        }
+
+        public void setDescricao(string descricao)
+        {
+            this.descricao = descricao;
+        }*/
+
+    }
+}
