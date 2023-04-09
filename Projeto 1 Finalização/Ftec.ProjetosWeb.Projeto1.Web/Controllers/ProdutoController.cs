8a9b8b183f3de2f9a7d584bcaca093d1540dad8d
Antonio Cardoso <antonioc_contas@hotmail.com>
2023-04-09 11:39:47 -0300

    Finlaização
    Correção da aula do projeto 1 da disciplina.
    
    Co-Authored-By: Fernando Martens <63741837+fernando-martens@users.noreply.github.com>

---
+++ Projeto 1 Finalização/Ftec.ProjetosWeb.Projeto1.Web/Controllers/ProdutoController.cs
@@ -0,0 +1,55 @@
+﻿using Ftec.ProjetosWeb.Projeto1.Web.Factory;
+using Ftec.ProjetosWeb.Projeto1.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ftec.ProjetosWeb.Projeto1.Web.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public IActionResult Index()
+        {
+            var produtos = ProdutoFactory.GeradorMoqProdutos(5);
+            ViewBag.produtos = produtos;
+            return View();
+        }
+
+        public IActionResult Inserir()
+        {
+            var categias = CategoriaProdutoFactory.GeradorMoqCategoriaProdutos(5);
+            ViewBag.categorias = categias;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Gravar(ProdutoModel produto)
+        {
+            //Faz a gravacao
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Excluir(Guid id)
+        {
+            //Faz a exclusão
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Editar(Guid id)
+        {
+            //Carrega o objeto a partir do ID passado
+            //Carrega uma View com os dados do objeto a serem alterados
+            var produto = new ProdutoModel()
+            {
+                Id = id,
+                CategoriaId = id,
+                Descricao = "Produto Teste",
+                Preco = 10,
+                QuantidadeEstoque = 10,
+                UnidadeMedida = "UN"
+            };
+
+            ViewBag.produto = produto;
+            ViewBag.categorias = CategoriaProdutoFactory.GeradorMoqCategoriaProdutos(5);
+            return View();
+        }
+    }
+}
---
+++ Projeto 1 Finalização/Ftec.ProjetosWeb.Projeto1.Web/Models/CategoriaProdutoModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Ftec.ProjetosWeb.Projeto1.Web.Models
+{
+    public class CategoriaProdutoModel
+    {
+        public Guid Id { get; set; }
+        public string Descricao { get; set; }
+    }
+}
